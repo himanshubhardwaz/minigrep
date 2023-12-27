347702ca0ee55201f054318e0f24560e1839a7e7
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-12-27 15:13:05 +0530

    tests and search logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!(
-        "Searching for {} in File contents: {}",
-        config.query, contents
-    );
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -26,5 +15,47 @@ impl Config {
         let filename = args[2].clone();
 
         return Ok(Config { query, filename });
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "road";
+
+        let contents = "\
+        this is a line which contains the word road
+        this line does not contain that word
+        ";
+
+        assert_eq!(
+            vec!["this is a line which contains the word road"],
+            search(query, contents)
+        );
     }
 }
